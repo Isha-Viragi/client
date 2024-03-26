9416773626c47b107cb1ccfc1c5086afc0472d4a
Isha Viragi <isha.viragi@gmail.com>
2024-03-25 20:45:21 -0400

    Initial Commit

---
+++ food-app/src/App.jsx
@@ -0,0 +1,12 @@
+import { useForm } from "react-hook-form";
+
+function App() {
+  return (
+    <>
+      <h1>Foodie</h1>
+      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
+    </>
+  );
+}
+
+export default App;
