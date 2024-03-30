0c780a926c309dbd4d8dc24e36bf32ae3681392e
Isha Viragi <isha.viragi@gmail.com>
2024-03-30 08:41:07 -0400

    Add NavBar and Footer components and Routes

--- food-app/src/App.jsx
+++ food-app/src/App.jsx
@@ -1,12 +1,26 @@
-import { useForm } from "react-hook-form";
+import React from "react";
+import NavBar from "./components/NavBar";
+import Footer from "./components/Footer";
+import Routers from "./services/Routers";
+import { Route, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import LoginPage from "./pages/LoginPage";
+import CartPage from "./pages/CartPage";
+import RegisterPage from "./pages/RegisterPage";
 
-function App() {
+const App = () => {
   return (
     <>
-      <h1>Foodie</h1>
-      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
+      <NavBar />
+      <Routes>
+        <Route exact path="/" element={<HomePage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/cart" element={<CartPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+      </Routes>
+      <Footer />
     </>
   );
-}
+};
 
 export default App;
