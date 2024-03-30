c5e3c2b06a657c0fbc32910d85e67cd3e58c798c
Isha Viragi <isha.viragi@gmail.com>
2024-03-30 08:36:40 -0400

    update NavBar component to use chakra ui and react-router-dom

--- food-app/src/components/NavBar.jsx
+++ food-app/src/components/NavBar.jsx
@@ -1,48 +1,38 @@
 import React from "react";
+import { Box, Grid, Heading } from "@chakra-ui/react";
+import { Link, NavLink } from "react-router-dom";
+import "../styles.css";
 
 const NavBar = () => {
   return (
-    <>
-      <nav className="navbar navbar-expand-lg px-5">
-        <div className="container-fluid">
-          <a className="navbar-brand" href="#">
-            <h1 className="text-warning fw-bold fs-3">FOODIE</h1>
-          </a>
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarNav"
-            aria-controls="navbarNav"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="d-flex justify-content-end">
-            <div className="collapse navbar-collapse" id="navbarNav">
-              <ul className="navbar-nav">
-                <li className="nav-item">
-                  <a className="nav-link active" aria-current="page" href="#">
-                    <h2 className="text-success fw-bold fs-5">Home</h2>
-                  </a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link" href="#">
-                    <h2 className="text-secondary fw-bold fs-5">Cart</h2>
-                  </a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link" href="#">
-                    <h2 className="text-secondary fw-bold fs-5">Profile</h2>
-                  </a>
-                </li>
-              </ul>
-            </div>
-          </div>
-        </div>
-      </nav>
-    </>
+    <Box bg="yellow.400" p={4}>
+      <Grid templateColumns="1fr auto" gap={4} alignItems="center">
+        {/* Grid for "Foodie" */}
+        <Grid templateColumns="1fr" alignItems="center">
+          <Heading color="orange.500" fontWeight="bold" size="lg">
+            <NavLink to="/">Foodie</NavLink>
+          </Heading>
+        </Grid>
+        {/* Grid for links */}
+        <Grid
+          templateColumns="repeat(3, auto)"
+          gap={4}
+          alignItems="center"
+          justifyItems="end"
+        >
+          <Heading mr={2} fontWeight="bold" color="white" size="md">
+            <NavLink to="/">Home</NavLink>
+          </Heading>
+          <Heading mr={2} fontWeight="bold" color="white" size="md">
+            <NavLink to="/cart">Cart</NavLink>
+          </Heading>
+          <Heading fontWeight="bold" color="white" size="md">
+            <NavLink to="/login">Profile</NavLink>
+          </Heading>
+        </Grid>
+      </Grid>
+    </Box>
   );
 };
+
 export default NavBar;
