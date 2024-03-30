fb9c1bd77e3bb7264063c32735f02d74232ed91a
Isha Viragi <isha.viragi@gmail.com>
2024-03-30 09:11:45 -0400

    Creating a responsive layout for HomePage

--- food-app/src/pages/HomePage.jsx
+++ food-app/src/pages/HomePage.jsx
@@ -1,9 +1,32 @@
+import { Grid, GridItem, Show } from "@chakra-ui/react";
 import React from "react";
+import NavBar from "../components/NavBar";
+import Footer from "../components/Footer";
 
 const HomePage = () => {
   return (
     <>
-      <h1>Home Page</h1>
+      <Grid
+        templateAreas={{
+          base: `"nav" "main" "footer"`,
+          lg: `"nav nav" "aside main" "footer footer"`,
+        }}
+      >
+        <GridItem area="nav" bg="coral">
+          <NavBar />
+        </GridItem>
+        <Show above="lg">
+          <GridItem area="aside" bg="gold">
+            Aside
+          </GridItem>
+        </Show>
+        <GridItem area="main" bg="dodgerblue">
+          Main
+        </GridItem>
+        <GridItem area="footer" bg="yellowgreen">
+          <Footer />
+        </GridItem>
+      </Grid>
     </>
   );
 };
