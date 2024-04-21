45c591afb1465b974d5fa48f0758bd8001c7ba3a
Isha Viragi <isha.viragi@gmail.com>
2024-04-20 22:39:06 -0400

    initial commit

--- food-app/src/components/Footer.jsx
+++ food-app/src/components/Footer.jsx
@@ -3,7 +3,7 @@ import { Box, Text } from "@chakra-ui/react";
 
 const Footer = () => {
   return (
-    <Box bg="gray.700" p={4} mt={4} textAlign="center">
+    <Box bg="gray.700" p={4} textAlign="center">
       <Text color="white">
         © All rights reserved to The Cabinet {new Date().getFullYear()}
       </Text>
