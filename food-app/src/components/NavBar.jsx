f9bb9bb8db2315d01e8656588745ad250420cd4d
Isha Viragi <isha.viragi@gmail.com>
2024-04-20 22:42:13 -0400

    verify token to navigate user profile

--- food-app/src/components/NavBar.jsx
+++ food-app/src/components/NavBar.jsx
@@ -3,14 +3,17 @@ import { Box, Grid, Heading } from "@chakra-ui/react";
 import { Link, NavLink } from "react-router-dom";
 import "../styles.css";
 
-const NavBar = () => {
+const NavBar = ({ onClickHome }) => {
+  const token = sessionStorage.getItem("x-auth-token");
   return (
     <Box bg="yellow.400" p={4}>
       <Grid templateColumns="1fr auto" gap={4} alignItems="center">
         {/* Grid for "Foodie" */}
         <Grid templateColumns="1fr" alignItems="center">
           <Heading color="orange.500" fontWeight="bold" size="lg">
-            <NavLink to="/">Foodie</NavLink>
+            <NavLink onClick={onClickHome} to="/">
+              Foodie
+            </NavLink>
           </Heading>
         </Grid>
         {/* Grid for links */}
@@ -21,13 +24,16 @@ const NavBar = () => {
           justifyItems="end"
         >
           <Heading mr={2} fontWeight="bold" color="white" size="md">
-            <NavLink to="/">Home</NavLink>
+            <NavLink onClick={onClickHome} to="/">
+              Home
+            </NavLink>
           </Heading>
           <Heading mr={2} fontWeight="bold" color="white" size="md">
             <NavLink to="/cart">Cart</NavLink>
           </Heading>
           <Heading fontWeight="bold" color="white" size="md">
-            <NavLink to="/login">Profile</NavLink>
+            {token && <NavLink to="/profile">Profile</NavLink>}
+            {!token && <NavLink to="/login">Profile</NavLink>}
           </Heading>
         </Grid>
       </Grid>
