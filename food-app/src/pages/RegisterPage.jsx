10de42e642f5eac7d02621cc458708127964d09f
Isha Viragi <isha.viragi@gmail.com>
2024-05-06 17:46:29 -0400

    use RegisterStepper component to display child components

--- food-app/src/pages/RegisterPage.jsx
+++ food-app/src/pages/RegisterPage.jsx
@@ -1,139 +1,140 @@
 import {
-  Button,
-  FormControl,
-  FormErrorMessage,
-  FormLabel,
-  Input,
+  Grid,
+  GridItem,
   Stack,
-  Heading,
+  Card,
+  CardBody,
+  Link,
+  Text,
+  Alert,
+  AlertIcon,
+  useToast,
 } from "@chakra-ui/react";
-import { zodResolver } from "@hookform/resolvers/zod";
-import React from "react";
-import { useForm } from "react-hook-form";
-import z from "zod";
+import PlainNavBar from "../components/PlainNavBar";
+import { NavLink, useNavigate } from "react-router-dom";
+import postRegister from "../hooks/postRegister";
+import { useEffect, useState } from "react";
+import RegisterStepper from "../components/RegisterStepper";
+import UserOption from "../components/UserOption";
+import PersonalInfoCard from "../components/PersonalInfoCard";
+import RegisterInfoCard from "../components/RegisterInfoCard";
+import RegisterAddressCard from "../components/RegisterAddressCard";
 
 const RegisterPage = () => {
-  const registerSchema = z.object({
-    firstName: z
-      .string()
-      .min(2, { message: "Please enter a valid name" })
-      .max(30, { message: "Please don't enter more than 30 characters" }),
+  const toast = useToast();
+  const [registerStatus, setRegisterStatus] = useState(false);
 
-    lastName: z
-      .string()
-      .min(3, { message: "Please enter a valid last name" })
-      .max(30, { message: "Please don't enter more than 30 characters" }),
+  const [nextStep, setNextStep] = useState(false);
+  const [formData, setFormData] = useState("");
+  const [data, setData] = useState("");
 
-    phoneNumber: z.string().regex(/^[0-9]{10,20}$/, {
-      message: "Please enter a valid phone number",
-    }),
+  const navigate = useNavigate();
 
-    address: z.object({
-      street: z
-        .string()
-        .min(3, { message: "Please enter a valid street name" }),
-      suite: z.string(),
-      city: z.string().min(3, { message: "Please enter a valid city" }),
-      province: z.string().min(2, { message: "Please enter a valid province" }),
-      zipcode: z
-        .string()
-        .min(6, { message: "Please enter a valid zipcode" })
-        .max(10),
-    }),
+  useEffect(() => {
+    if (nextStep) {
+      setNextStep(false);
+    }
+  }, [nextStep]);
 
-    email: z.string().min(4, { message: "Please enter a valid email" }),
-    password: z
-      .string()
-      .min(6, { message: "Password must be atleast 6 chracters" })
-      .max(50, { message: "Password must be at most 50 characters long" }),
-  });
+  useEffect(() => {
+    if (registerStatus) {
+      const { confirmPassword, ...data } = formData;
+      setData(data);
+    }
+  }, [registerStatus]);
 
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm({ resolver: zodResolver(registerSchema) });
+  const { result, error } = postRegister({ data });
 
-  const onSubmit = (data) => {
-    console.log(data);
-  };
+  if (result) {
+    toast({
+      title: "Success",
+      description: "Welcome to our family",
+      status: "success",
+      duration: 7000,
+      isClosable: true,
+    });
+  }
+
+  if (error) {
+    toast({
+      title: "Error",
+      description: error.response.data,
+      status: "error",
+      duration: 7000,
+      isClosable: true,
+    });
+  }
+
+  useEffect(() => {
+    if (result) {
+      const token = result.headers["x-auth-token"];
+      sessionStorage.setItem("x-auth-token", token);
+      navigate("/");
+    }
+  }, [result]);
 
   return (
     <>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <Stack spacing={4} m={4}>
-          <Heading mt={2} as="h1" size="md" mx={4} align="center">
-            Personal Details
-          </Heading>
-          <FormControl id="firstName" isInvalid={errors.firstName}>
-            <FormLabel>First Name</FormLabel>
-            <Input {...register("firstName")} type="text" />
-            <FormErrorMessage>{errors.firstName?.message}</FormErrorMessage>
-          </FormControl>
-          <FormControl id="lastName" isInvalid={errors.lastName}>
-            <FormLabel>Last Name</FormLabel>
-            <Input {...register("lastName")} type="text" />
-            <FormErrorMessage>{errors.lastName?.message}</FormErrorMessage>
-          </FormControl>
-          <FormControl id="phoneNumber" isInvalid={errors.phoneNumber}>
-            <FormLabel>Phone Number</FormLabel>
-            <Input {...register("phoneNumber")} type="tel" />
-            <FormErrorMessage>{errors.phoneNumber?.message}</FormErrorMessage>
-          </FormControl>
+      <Grid templateAreas={`"nav" "main"`}>
+        <GridItem area="nav">
+          <PlainNavBar />
+        </GridItem>
+        <GridItem area="main">
+          <Stack align="stretch" m={10}>
+            <RegisterStepper
+              onNextClick={nextStep}
+              stepOne={
+                <UserOption
+                  onSelectRestaurant={() => {
+                    setNextStep(true);
+                    setFormData({ role: "restaurantUser" });
+                  }}
+                  onSelectUser={() => {
+                    setNextStep(true);
+                    setFormData({ role: "user" });
+                  }}
+                />
+              }
+              stepTwo={
+                <PersonalInfoCard
+                  onNextStep={(data) => {
+                    setFormData((prevData) => ({ ...prevData, ...data }));
+                    setNextStep(true);
+                  }}
+                />
+              }
+              stepThree={
+                <RegisterInfoCard
+                  onNextStep={(data) => {
+                    setFormData((prevData) => ({
+                      ...prevData,
+                      ...data,
+                    }));
+                    setNextStep(true);
+                  }}
+                />
+              }
+              stepFour={
+                <RegisterAddressCard
+                  onSubmit={(data) => {
+                    setFormData((prevData) => ({ ...prevData, ...data }));
+                    setRegisterStatus(true);
+                  }}
+                />
+              }
+            />
 
-          <FormControl id="email" isInvalid={errors.email}>
-            <FormLabel>Email</FormLabel>
-            <Input {...register("email")} type="email" />
-            <FormErrorMessage>{errors.email?.message}</FormErrorMessage>
-            <FormControl id="password" isInvalid={errors.password}>
-              <FormLabel>Password</FormLabel>
-              <Input {...register("password")} type="password" />
-              <FormErrorMessage>{errors.password?.message}</FormErrorMessage>
-            </FormControl>
-          </FormControl>
-
-          <Heading mt={2} as="h1" size="md" mx={4} align="center">
-            Address
-          </Heading>
-          <FormControl id="street" isInvalid={errors.address?.street}>
-            <FormLabel>Street</FormLabel>
-            <Input {...register("address.street")} type="text" />
-            <FormErrorMessage>
-              {errors.address?.street?.message}
-            </FormErrorMessage>
-          </FormControl>
-          <FormControl id="suite" isInvalid={errors.address?.suite}>
-            <FormLabel>Suite</FormLabel>
-            <Input {...register("address.suite")} type="text" />
-            <FormErrorMessage>
-              {errors.address?.suite?.message}
-            </FormErrorMessage>
-          </FormControl>
-          <FormControl id="city" isInvalid={errors.address?.city}>
-            <FormLabel>City</FormLabel>
-            <Input {...register("address.city")} type="text" />
-            <FormErrorMessage>{errors.address?.city?.message}</FormErrorMessage>
-          </FormControl>
-          <FormControl id="province" isInvalid={errors.address?.province}>
-            <FormLabel>Province</FormLabel>
-            <Input {...register("address.province")} type="text" />
-            <FormErrorMessage>
-              {errors.address?.province?.message}
-            </FormErrorMessage>
-          </FormControl>
-          <FormControl id="zipcode" isInvalid={errors.address?.zipcode}>
-            <FormLabel>Zipcode</FormLabel>
-            <Input {...register("address.zipcode")} type="text" />
-            <FormErrorMessage>
-              {errors.address?.zipcode?.message}
-            </FormErrorMessage>
-          </FormControl>
-
-          <Button type="submit" mt={4}>
-            Submit
-          </Button>
-        </Stack>
-      </form>
+            <Stack mt={4} align="center">
+              <NavLink to="/login">
+                Already have an account?{" "}
+                <Link color="blue" as="span">
+                  Log In!
+                </Link>
+              </NavLink>
+            </Stack>
+          </Stack>
+        </GridItem>
+      </Grid>
     </>
   );
 };
