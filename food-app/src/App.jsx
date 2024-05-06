15735a33a23cbb2b80e39e296778f0909d575ed7
Isha Viragi <isha.viragi@gmail.com>
2024-05-06 18:05:22 -0400

    add RestaurantPage routes

--- food-app/src/App.jsx
+++ food-app/src/App.jsx
@@ -1,24 +1,27 @@
 import React from "react";
-import NavBar from "./components/NavBar";
-import Footer from "./components/Footer";
-import Routers from "./services/Routers";
 import { Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import LoginPage from "./pages/LoginPage";
 import CartPage from "./pages/CartPage";
 import RegisterPage from "./pages/RegisterPage";
+import ProfilePage from "./pages/ProfilePage";
+import RestaurantPage from "./pages/RestaurantPage";
+import NotFoundPage from "./pages/PageNotFound";
 
 const App = () => {
   return (
     <>
-      <NavBar />
       <Routes>
         <Route exact path="/" element={<HomePage />} />
+        <Route exact path="/restaurant/:id" element={<RestaurantPage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/cart" element={<CartPage />} />
         <Route path="/register" element={<RegisterPage />} />
+        <Route path="/profile" element={<ProfilePage />} />
+        <Route path="/error" element={<NotFoundPage />} />
+
+        {/* <Route path="/register" element={<UserOptionPage />} /> */}
       </Routes>
-      <Footer />
     </>
   );
 };
